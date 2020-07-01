60a19a8074c051daf6c9cab97a0cac8fb8f44afb
Khien <Khien@Khien-PC>
2020-07-01 23:17:40 +0800

    DequeueQuiz5

--- src/dequeexample/DequeExample.java
+++ src/dequeexample/DequeExample.java
@@ -21,52 +21,54 @@ public class DequeExample {
     public static void main(String[] args) {
 
         // TODO code application logic here
-        DNode<String> node1 = new DNode<>(null, "John", null);
-        DNode<String> node2 = new DNode<>(null, "Dominique", null);
-        DNode<String> node3 = new DNode<>(null, "Kyle", null);
-        DNode<String> node4 = new DNode<>(null, "Shaun", null);
-        DNode<String> node5 = new DNode<>(null, "Jerson", null);
+        DNode<String> transact1 = new DNode<>(null, "John", null);
+        DNode<String> transact2 = new DNode<>(null, "Dominique", null);
+        DNode<String> transact3 = new DNode<>(null, "Kyle", null);
+        DNode<String> transact4 = new DNode<>(null, "Shaun", null);
+        DNode<String> transact5 = new DNode<>(null, "Jerson", null);
 
-        DequeList<DNode> list = new DequeList<>();
-        list.addFirst(node1);
-        list.addFirst(node2);
-        list.addFirst(node3);
-        list.addFirst(node4);
-        list.addLast(node5);
-
-        try {
+        
+        DequeList<DNode>transaction = new DequeList<>();
+     
+        transaction.addFirst(transact1);
+       transaction.addFirst(transact2);
+        transaction.addFirst(transact3);
+        transaction.addFirst(transact4);
+        transaction.addFirst(transact5);
+               
+       try {
             System.out.println("Before Remove: ");
-            list.showDeque();
+            transaction.showDeque();
 
             System.out.println();
-            System.out.println("Remove First: " + list.removeFirst().getElement());
+            System.out.println("Remove First: " + transaction.removeFirst().getElement());
             System.out.print("After Remove: ");
-            list.showDeque();
+            transaction.showDeque();
 
             System.out.println();
-            System.out.println("Remove Last: " + list.removeLast().getElement());
+            System.out.println("Remove Last: " + transaction.removeLast().getElement());
             System.out.print("After Remove: ");
-            list.showDeque();
+            transaction.showDeque();
 
             System.out.println();
-            System.out.println("Remove Last: " + list.removeLast().getElement());
+            System.out.println("Remove Last: " + transaction.removeLast().getElement());
             System.out.print("After Remove: ");
-            list.showDeque();
+            transaction.showDeque();
 
             System.out.println();
-            System.out.println("Remove Last: " + list.removeLast().getElement());
+            System.out.println("Remove Last: " + transaction.removeLast().getElement());
             System.out.print("After Remove: ");
-            list.showDeque();
+            transaction.showDeque();
 
             System.out.println();
-            System.out.println("Remove Last: " + list.removeLast().getElement());
+            System.out.println("Remove Last: " + transaction.removeLast().getElement());
             System.out.print("After Remove: ");
-            list.showDeque();
+            transaction.showDeque();
 
             System.out.println();
-            System.out.println("Remove Last: " + list.removeLast().getElement());
+            System.out.println("Remove Last: " + transaction.removeLast().getElement());
             System.out.print("After Remove: ");
-            list.showDeque();
+            transaction.showDeque();
         } catch (EmptyDequeException ex) {
             System.out.println(ex.getMessage());
         }
